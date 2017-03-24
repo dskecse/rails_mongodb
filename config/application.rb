483862aaad6943658ae90942c1b20141825cb014
Dzianis Dashkevich <dskecse@gmail.com>
2017-03-24 17:34:00 +0300

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,10 @@ Bundler.require(*Rails.groups)
 
 module RailsMongodb
   class Application < Rails::Application
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+      g.view_specs = false
+    end
   end
 end
